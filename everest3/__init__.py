38749f34948d38d2ed95eccf482df4b7f66efd7d
Rodrigo Luger <rodluger@gmail.com>
2017-09-01 17:30:29 -0700

    boilerplate

--- everest3/__init__.py
+++ everest3/__init__.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import division, print_function, absolute_import, unicode_literals
-
-# Version number
+# Version
 __version__ = "3.0.0"
+__author__ = "Rodrigo Luger (rodluger@uw.edu)"
+__copyright__ = "Copyright 2017 Rodrigo Luger"
 
-from . import pld
-from . import mission
+# Was everest3 imported from setup.py?
+try:
+  __EVEREST3_SETUP__
+except NameError:
+  __EVEREST3_SETUP__ = False
+
+if not __EVEREST3_SETUP__:
+
+    from . import pld
+    from . import mission
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, absolute_import
+from setuptools import setup, find_packages, Extension
+import glob
+    
+# Hackishly inject a constant into builtins to enable importing of the
+# module in "setup" mode. Stolen from `kplr`
+import sys
+if sys.version_info[0] < 3:
+  import __builtin__ as builtins
+else:
+  import builtins
+builtins.__EVEREST3_SETUP__ = True
+import everest3
+
+long_description = \
+"""
+EVEREST 3.0
+"""
+
+# Setup!
+setup(name = 'everest3',
+      version = everest3.__version__,
+      description = 'EVEREST 3.0',
+      long_description = long_description,
+      classifiers = [
+                      'Development Status :: 3 - Alpha',
+                      'License :: OSI Approved :: MIT License',
+                      'Programming Language :: Python',
+                      'Programming Language :: Python :: 3',
+                      'Topic :: Scientific/Engineering :: Astronomy',
+                    ],
+      url = 'http://github.com/rodluger/everest3',
+      author = 'Rodrigo Luger',
+      author_email = 'rodluger@uw.edu',
+      license = 'MIT',
+      packages = ['everest3'],
+      install_requires = [
+                          'numpy>=1.8',
+                          'scipy',
+                          'matplotlib',
+                          'six'
+                         ],
+      include_package_data = True,
+      zip_safe = False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
