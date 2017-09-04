fb4944c6b4371903fb76645c6a9d0f646fb5be53
Rodrigo Luger <rodluger@gmail.com>
2017-09-04 08:39:12 -0700

    added tests folder

---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+test_main.py
+------------
+
+'''
+
+from __future__ import division, print_function, absolute_import, unicode_literals
+
+def test_main():
+    '''
+    
+    '''
+    
+    pass
